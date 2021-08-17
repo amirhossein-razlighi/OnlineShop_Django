3cb525c5cbd1dc91f172ac4339a8b252eed873f3
AmirHossein-nr <arazlighi@gmail.com>
2021-08-17 15:44:19 +0430

    - Edit home page (BootStrap Cards) - Add product page

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from shop.models import Product
 
 
-# Create your views here.
 def home(request):
-    return render(request, 'shop/home.html')
+    products = Product.objects.filter(is_available=True)
+    return render(request, 'shop/home.html', {'products': products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'shop/product_detail.html', {'product': product})
